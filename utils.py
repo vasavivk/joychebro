4636c8e71c43d8f77d5e6937fd69d7f98049a5e2
Dileepoke <85066181+Dileepoke@users.noreply.github.com>
2023-07-29 18:29:55 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -84,7 +84,7 @@ def get_account_details(email, password, app_id):
     url = f"https://www.qobuz.com/api.json/0.2/user/login?email={email}&password={password}&app_id={app_id}"
     response = requests.get(url, headers={'User-Agent': USER_AGENT})
     res = response.json()
-     if 'status' not in res or 'end_date' and 'credential' in res :
+    if 'status' not in res or 'end_date' and 'credential' in res :
       user = res['user']
       userid = user['id']
       auth_token = res['user_auth_token']
